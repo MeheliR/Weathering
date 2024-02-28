7efc2c6ea505182b1ecdd104040f2dbaa6f1b08d
MeheliR <meheli2022roy@gmail.com>
2024-02-28 10:00:05 +0530

    error day

--- more/assest/js/module.js
+++ more/assest/js/module.js
@@ -1,3 +1,4 @@
+
 'use strict';
 export const weekDayNames=[
     "Sunday",
@@ -26,8 +27,8 @@ export const monthNames=[
 export const getDate = (dateUnix,timezone)=>{
     const date = new Date((dateUnix+timezone)*1000);
     // Adjusting UTC day index to match weekDayNames array
-    // const weekDayName = weekDayNames[(date.getUTCDay() + 6) % 7];
-    const weekDayName = weekDayNames[date.getUTCDay()];
+    const weekDayName = weekDayNames[(date.getUTCDay() + 6) % 7];
+    //const weekDayName = weekDayNames[date.getUTCDay()];
     const monthName = monthNames[date.getUTCMonth()];
 
     return `${weekDayName} ${date.getUTCDate()} ${monthName}`;
